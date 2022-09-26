c2e4c9e6f7728bf2a2d94df1d63924107f27913e
gemaquejr <gemaquejr@hotmail.com>
2022-09-26 15:49:56 -0300

    Adiciona componente solar system

---
+++ solar-system/src/components/SolarSystem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="solar-system" data-testid="solar-system" />
+        <Title headline="Planetas" />
+        {planets.map((planet) => (
+          <PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
+      </>
+    );
+  }
+}
+
+export default SolarSystem;
